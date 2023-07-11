03eb17644ce847f692609eacc3bcba0a716cb96b
Samet Yigitkalan <sametykalan@gmail.com>
2023-07-11 16:19:19 +0300

    finish character movement and rotation

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -7,30 +7,28 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    [SerializeField] LeanJoystick moveJoystick;
-    public Vector2 moveDirection { get; private set; }
-    public Vector2 aimDirection { get; private set; }
-    [SerializeField] LeanJoystick aimJoystick;
-    // Start is called before the first frame update
-    void Start()
-    {
+	[SerializeField]
+	LeanJoystick moveJoystick;
+	public Vector2 movementInput { get; private set; }
 
-        Observable.EveryUpdate().Subscribe(
-            _ => SetDirections()
-        ).AddTo(this);
+	[SerializeField]
+	LeanJoystick aimJoystick;
+	public Vector2 aimInput { get; private set; }
 
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		Observable.EveryUpdate().Subscribe(_ => SetDirections()).AddTo(this);
+	}
 
+	void SetDirections()
+	{
+		movementInput = moveJoystick.ScaledValue;
+		aimInput = aimJoystick.ScaledValue;
+	}
 
-
-    void SetDirections()
-    {
-        moveDirection = moveJoystick.ScaledValue;
-        aimDirection = aimJoystick.ScaledValue;
-    }
-    bool IsAiming()
-    {
-        return aimJoystick.ScaledValue != Vector2.zero;
-    }
-
+	public bool IsAiming()
+	{
+		return aimJoystick.ScaledValue != Vector2.zero;
+	}
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,30 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System;
 using UniRx;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    PlayerInput _playerInput;
-    [SerializeField] private float speed = 10;
+	PlayerInput _playerInput;
+	Rigidbody rb;
 
-    void Start()
-    {
-        _playerInput = GetComponent<PlayerInput>();
+	[Header("Movement")]
+	[SerializeField]
+	private float moveSpeed = 4;
+	Vector3 movementVector;
 
+	[Header("Aim")]
+	[SerializeField]
+	float aimSmoothTime = 0.1f;
 
-        Observable.EveryUpdate().Subscribe(_ =>
-        {
-            Move();
-        });
+	Vector3 aimVector;
 
-    }
+	IDisposable movementDisposable;
 
-    void Move()
-    {
-        transform.Translate(_playerInput.moveDirection * speed * Time.deltaTime, Space.World);
-    }
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		_playerInput = GetComponent<PlayerInput>();
+	}
 
+	void Start()
+	{
+		movementDisposable = Observable
+			.EveryFixedUpdate()
+			.Where(
+				_ =>
+					_playerInput.movementInput != Vector2.zero
+					|| _playerInput.aimInput != Vector2.zero
+			)
+			.Subscribe(_ =>
+			{
+				MovePlayerOnXZ();
+				RotatePlayer();
+			});
+	}
 
+	private void OnDisable()
+	{
+		movementDisposable.Dispose();
+	}
+
+	void MovePlayerOnXZ()
+	{
+		movementVector = new Vector3(
+			_playerInput.movementInput.x,
+			0,
+			_playerInput.movementInput.y
+		);
+
+		transform.Translate(
+			movementVector * moveSpeed * Time.deltaTime,
+			Space.World
+		);
+	}
+
+	void RotatePlayer()
+	{
+		if (_playerInput.IsAiming())
+		{
+			aimVector = new Vector3(
+				_playerInput.aimInput.x,
+				0,
+				_playerInput.aimInput.y
+			);
+			transform.rotation = Quaternion.Slerp(
+				transform.rotation,
+				Quaternion.LookRotation(aimVector, Vector3.up),
+				aimSmoothTime * Time.deltaTime
+			);
+		}
+		else
+		{
+			transform.rotation = Quaternion.Slerp(
+				transform.rotation,
+				Quaternion.LookRotation(movementVector, Vector3.up),
+				aimSmoothTime * Time.deltaTime
+			);
+		}
+	}
 }
